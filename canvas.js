146b6d9eab03b50cb7181f0b15bcbfd12f1778a5
Dhiraj Govindvira <dhirajgovindvira1112@gmail.com>
2021-09-13 12:47:16 +0530

    finished.

--- canvas.js
+++ canvas.js
@@ -1,7 +1,11 @@
+const editorEle = document.getElementById("editor");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
+
+let current_color = "#222222";
 canvas.height = window.innerHeight;
-canvas.width = window.innerWidth / 2;
+canvas.width = window.innerWidth - editorEle.clientWidth;
+const bound = canvas.getBoundingClientRect();
 
 let painting = false;
 function startPosition(e) {
@@ -14,9 +18,16 @@ function finishPosition() {
 }
 function draw(e) {
     if(!painting) return;
-    ctx.lineWidth = 5;
+    if(current_color == "#fff") {
+        ctx.lineWidth = 30;
+    }
+    else {
+        ctx.lineWidth = 3;
+    }
     ctx.lineCap = 'round';
-    ctx.lineTo(e.clientX, e.clientY);
+    let x = e.clientX - bound.left;
+    let y = e.clientY - bound.top;
+    ctx.lineTo(x, y);
     ctx.stroke();
 }
 canvas.addEventListener('mousedown', startPosition);
@@ -24,9 +35,12 @@ canvas.addEventListener('mouseup', finishPosition);
 canvas.addEventListener('mousemove', draw);
 
 function setClr(clr) {
+    console.log(clr);
     ctx.strokeStyle = clr;
+    current_color = clr;
 }
 
-function clear() {
-
+function clean() {
+    console.log("clear");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
--- editor.js
+++ editor.js
@@ -6,13 +6,42 @@ editor.setOptions({
    fontSize: "20px"
 });
 editor.setTheme("ace/theme/monokai");
-// editor.session.setMode("ace/mode/c_cpp");
+
+let cur_lang = "text";
+let cur_font_size = "20px";
+
+if (typeof(Storage) !== "undefined") {
+    let code = localStorage.getItem("code");
+    let lang = localStorage.getItem("lang");
+    let fontSize = localStorage.getItem("font-size");
+    if(code) {
+        editor.setValue(code);
+    }
+    if(lang) {
+        setLanguage(lang);
+    }
+    if(fontSize) {
+        setFont(fontSize);
+    }
+}
 
 function setFont(sz) {
     console.log(sz);
+    cur_font_size = sz;
     editor.setFontSize(`${sz}px`);
 }
 function setLanguage(lang) {
     console.log(lang);
+    cur_lang = lang;
     editor.session.setMode(`ace/mode/${lang}`);
 }
+
+function save() {
+    let code = editor.getValue();
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("code", code);
+        localStorage.setItem("lang", cur_lang);
+        localStorage.setItem("font-size", cur_font_size);
+    }
+}
+setInterval(save, 5000);
